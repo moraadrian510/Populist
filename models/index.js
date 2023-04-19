01d43836f554f95b8d38faaa0c173a2d96aa1caf
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-04-18 19:03:35 -0700

    updated models/user

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./user');
+const Thoughts = require('./Thoughts');
+
+
+
+module.exports= { User, Thoughts };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const connectionStringURI = "mongodb://127.0.0.1:27017";
 const dbName = "populistDB";
 
