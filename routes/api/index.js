0546d7127b725395fddbf2598e0315b921110ce8
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-04-18 22:25:31 -0700

    save

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/users', userRoutes);
-router.use('/thoughts, thoughtRoutes');
+router.use('/thoughts', thoughtRoutes);
 
 
 
