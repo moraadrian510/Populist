4f39d70938a8270fef8827fd12d35f04a81f8c94
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-04-18 22:23:59 -0700

    updated models/thoughts && api index

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,1 +1,31 @@
+const { Schema, model } = require('mongoose');
 
+
+const thoughtSchema = new Schema(
+{
+    thoughtText: {
+        type:  String,
+        required: true,
+        minlength: 1,
+        maxlength: 255,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+}
+);
+
+thoughtSchema.pre('remove', function(next){
+    this.model('User').updateOne(
+        { thoughts: this._id },
+        { $pull: { thoughts: this._id } },
+        { multi: true },
+        next
+    );
+});
+
+const Thoughts = model('thoughts', thoughtSchema);
+
+module.exports = Thoughts;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,11 @@
 const User = require('./user');
 const Thoughts = require('./Thoughts');
 
-
+// SChema to create thoughts
+const thoughtsSchema = new Schema(
+    {
+        
+    }
+)
 
 module.exports= { User, Thoughts };
