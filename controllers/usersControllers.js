3325b9ae14dd4df42044a8472910be767e93006b
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-04-18 23:58:03 -0700

    userController/gett single/ done

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,17 +1,33 @@
-const { User, Thoughts } = require('../models');
-const { ObjecId } = require('mongoose').Types;
+const { User, Thoughts } = require("../models");
+const { ObjecId } = require("mongoose").Types;
 
 module.exports = {
-    //get all users with their thoughts
-    getUsers(req, res) {
+  //get all users with their thoughts
+  getUsers(req, res) {
     User.find()
-        .populate('thoughts')
-        .then(users => {
+      .populate("thoughts")
+      .then((users) => {
         res.json(users);
-        })
-        .catch(err => {
+      })
+      .catch((err) => {
         console.log(err);
         res.status(500).json(err);
-        });
-    }
-}
+      });
+  },
+  // get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .then(async (users) => {
+        !users
+          ? res.status(404).json({ message: "User not found with that ID" })
+          : res.json({
+              message: "User found",
+            });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json;
+      });
+  },
+};
