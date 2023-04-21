608a02b61e57d45cc73fa6fcf2612ca61c0378a6
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-04-21 12:42:21 -0700

    created Reaction schema sub document

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -7,19 +7,35 @@ const thoughtSchema = new Schema(
         type:  String,
         required: true,
         minlength: 1,
-        maxlength: 255,
+        maxlength: 280,
     },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-   createdAt: {
+    createdAt: {
         type: Date,
-        default: Date.now
-   },
-}, { timestamps: true });
+        default: Date.now,
+        get: function (createdAt) {
+            // formast the time stamp to a more readable string
+            return new Date(createdAt).toDateString();
+        }
+    },
+    userName: {
+    type: String,
+    ref: 'User',
+    required: true,
+    },
+    reactions: [reactionSchema],
 
+}, 
+{ timestamps: true ,
+    toJSON: {
+        virtuals: true,
+    }
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
+//cascade effect to delete thoughts when user is deleted
 thoughtSchema.pre('remove', function(next){
     this.model('User').updateOne(
         { thoughts: this._id },
@@ -29,6 +45,6 @@ thoughtSchema.pre('remove', function(next){
     );
 });
 
-const Thoughts = model('Thought', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
-module.exports = Thoughts;
+module.exports = Thought;
