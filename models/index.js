4d1f153391117966fbd03676b8beab01e4c3fa64
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-04-23 21:38:39 -0700

    Reaction schema defie bug

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./user');
 const Thoughts = require('./Thoughts');
+const Reaction = require('./Reaction')
 
 
 
-module.exports= { User, Thoughts };
+module.exports= { User, Thoughts, Reaction };
